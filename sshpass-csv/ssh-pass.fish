4ddade124a7fc057d621678773d90e2c42daae6c
dagimal <daffagifariakmal@gmail.com>
2023-09-30 14:31:03 +0700

    add sshpass csv

---
+++ sshpass-csv/ssh-pass.fish
@@ -0,0 +1,16 @@
+function icon
+    set filePath "/home/dagimal/.ssh/icon.csv"
+    set name (awk -F',' '{print $1, "("$2"@"$3")"}' $filePath | fzf)
+    
+    # Variable Check
+    if test -n "$name"
+        set user (grep "^$name," $filePath | awk -F',' '{print $2}')
+        set host (grep "^$name," $filePath | awk -F',' '{print $3}')
+        set pass (grep "^$name," $filePath | awk -F',' '{print $4}')
+        
+        echo "SSH session started, connecting to host: $host, user: $user", name: $name
+	/usr/bin/sshpass -p $pass ssh $user@$host
+    else
+        echo "No host selected."
+    end
+end
