4f9b005a85ab1865bd1a3a0dd1701fe653eee1cd
dagimal <daffagifariakmal@gmail.com>
2023-10-08 12:12:09 +0700

    exclude # symbol from fzf

--- sshpass-csv/ssh-pass.fish
+++ sshpass-csv/ssh-pass.fish
@@ -1,15 +1,15 @@
 function icon
     set filePath "/home/dagimal/.ssh/icon.csv"
-    set title (awk -F',' '{print $1, "("$2"@"$3")"}' $filePath | fzf)
+    set title (awk -F',' '{print $1, "("$2"@"$3")"}' $filePath | grep -v '#' | fzf)
     set name (string split ' ' $title)[1]
-    # Variable Check
+    
     if test -n "$name"
         set user (grep "^$name," $filePath | awk -F',' '{print $2}')
         set host (grep "^$name," $filePath | awk -F',' '{print $3}')
         set pass (grep "^$name," $filePath | awk -F',' '{print $4}')
         
-        echo "SSH session started, connecting to host: $host, user: $user", name: $name
-	/usr/bin/sshpass -p $pass ssh $user@$host
+        echo "SSH session started, connecting to host: $host, user: $user, name: $name"
+        /usr/bin/sshpass -p $pass ssh $user@$host
     else
         echo "No host selected."
     end
