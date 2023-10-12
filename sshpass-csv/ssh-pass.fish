cf7b954347440a562d6de248f3c25abb45cacd30
dagimal <daffagifariakmal@gmail.com>
2023-10-12 19:45:51 +0700

    add function to check port

--- sshpass-csv/ssh-pass.fish
+++ sshpass-csv/ssh-pass.fish
@@ -7,9 +7,15 @@ function icon
         set user (grep "^$name," $filePath | awk -F',' '{print $2}')
         set host (grep "^$name," $filePath | awk -F',' '{print $3}')
         set pass (grep "^$name," $filePath | awk -F',' '{print $4}')
+        set port (grep "^$name," $filePath | awk -F',' '{print $5}')
+
+        # Check if $port is empty and set it to 22 if it is
+        if test -z "$port"
+            set port 22
+        end
         
-        echo "SSH session started, connecting to host: $host, user: $user, name: $name"
-        /usr/bin/sshpass -p $pass ssh $user@$host
+        echo "SSH session started, connecting to host: $host, user: $user, name: $name, port: $port"
+        /usr/bin/sshpass -p $pass ssh $user@$host -p $port
     else
         echo "No host selected."
     end
